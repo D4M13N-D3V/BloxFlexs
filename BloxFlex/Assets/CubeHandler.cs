be7ce408c42b7d50627910f1102a87314b459af9
Trinket Entertainment <damienostler1@outlook.com>
2015-09-25 14:53:17 -0400

    Menu
    Menu

---
+++ BloxFlex/Assets/CubeHandler.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeHandler : MonoBehaviour {
+    public float speed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = new Vector3(transform.position.x - speed, transform.position.y, transform.position.z);
+	}
+
+    public IEnumerator destroySelf(float time)
+    {
+        yield return new WaitForSeconds(time);
+        Destroy(gameObject);
+    }
+}
---
+++ BloxFlex/Assets/CubeSpawnManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CubeSpawnManager : MonoBehaviour {
+    public GameObject[] cubeVarietys;
+    public GameObject[] cubeSpawnPoints;
+    public float cubeSpeed;
+    public float selfDestroyTime;
+    public float cubeSpawnRate;
+
+	void Start () {
+        StartCoroutine(spawnCubeTimer());
+	}
+	
+	void Update () {
+	
+	}
+
+    IEnumerator spawnCubeTimer()
+    {
+        while (true)
+        {
+            yield return new WaitForSeconds(cubeSpawnRate);
+            spawnCube();
+            spawnCube();
+            spawnCube();
+        }
+    }
+
+    public void spawnCube()
+    {
+        int rng = Random.Range(0, cubeSpawnPoints.Length);
+        Vector3 spawnpos = cubeSpawnPoints[rng].transform.position;
+        Vector3 angles = cubeSpawnPoints[rng].transform.eulerAngles;
+        rng = Random.Range(0, cubeVarietys.Length);
+        GameObject temp = Instantiate(cubeVarietys[rng], spawnpos, Quaternion.identity) as GameObject;
+        temp.transform.eulerAngles = angles;
+        temp.GetComponent<CubeHandler>().speed = cubeSpeed;
+        StartCoroutine(temp.GetComponent<CubeHandler>().destroySelf(selfDestroyTime));
+    }
+}
