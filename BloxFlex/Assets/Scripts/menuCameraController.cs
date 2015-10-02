e0e568ef207352d7ca8cd8482f23b2b3150906ea
DrCreo <DrCreo1@gmail.com>
2015-10-02 18:03:44 +0100

    Added main menu Camera rotations

---
+++ BloxFlex/Assets/Scripts/menuCameraController.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class menuCameraController : MonoBehaviour 
+{
+public int state;
+public Vector3[] rotations;
+public Vector3[] posistions;
+
+// Use this for initialization
+void Start()
+{
+
+}
+
+// Update is called once per frame
+void Update()
+{
+    transform.eulerAngles = rotations[state];
+    transform.position = posistions[state];
+}
+}
